a204229c12f1e1e0e9aea21955e4d3d26dd6e936
abdelmoniem <3bdelmoniemhamed11@gmail.com>
2018-09-17 04:51:50 +0200

    template

---
+++ template 1 with html,css,javascript/javascript.js
@@ -0,0 +1,20 @@
+function hoverbutton(){
+    "use strict";
+    
+    document.getElementById("button").style.backgroundColor="rgb(0,188,156)";
+};
+function mouseleave(){
+    "use strict";
+    document.getElementById("button").style.backgroundColor=" #00bc80";
+    
+}
+function hoverbutton2(){
+    "use strict";
+    
+    document.getElementById("button2").style.backgroundColor="rgb(235, 228, 228) ";
+};
+function mouseleave2(){
+    "use strict";
+    document.getElementById("button2").style.backgroundColor=" white";
+    
+}
